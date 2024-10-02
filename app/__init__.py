6203a469c94772cf249d19ddf3cb291390b20785
Ivan Kayukov <mr.lipova@list.ru>
2024-10-02 17:12:55 +0300

    реализовал структуру бд

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,21 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from config import Config
+from app import routes, models
 
-app = Flask(__name__)
-app.config.from_object(Config)
-
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-login = LoginManager(app)
+db = SQLAlchemy()
+migrate = Migrate()
+login = LoginManager()
 login.login_view = 'login'
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login.init_app(app)
+
+    from app import routes, models
+    
+    return app
--- app/models.py
+++ app/models.py
@@ -1,11 +1,43 @@
 from app import db, login
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    transactions = db.relationship('Transaction', backref='user', lazy='dynamic')
+    budgets = db.relationship('Budget', backref='user', lazy='dynamic')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Float, nullable=False)
+    description = db.Column(db.String(140))
+    date = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    income = db.Column(db.Intiger, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.Foreignkey('user_id'))
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True)
+    transaction = db.relationship('Transaction', backref='category', lazy='dynamic')
+
+class Budget(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Float, nullable=False)
+    start_date = db.Column(db.DateTime, nullable=False)
+    end_date = db.Column(db.DateTime, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 @login.user_loader
 def load_user(id):
