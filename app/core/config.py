f2e6d30c97759222ef4f14f72ff95e19d7d0daef
Ivan Kayukov <mr.lipova@list.ru>
2024-10-08 17:15:52 +0300

    minor fix

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_URL: str = 'postgresql://user:password@localhost/dbname'
-    SECRET_KEY: str = 'your-secret-key'
+    SECRET_KEY: str = 'my-secret-key'
     ALGORITHM: str = 'HS256'
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
